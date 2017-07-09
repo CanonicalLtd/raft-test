098626e5029526e4203f1089106faa6e3b4768a8
Free Ekanayaka <free.ekanayaka@canonical.com>
2017-07-09 10:51:20 +0200

    Add Cluster helper for managing a group of test peer nodes

---
+++ cluster.go
@@ -0,0 +1,242 @@
+// Copyright 2017 Canonical Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rafttest
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/hashicorp/raft"
+)
+
+// Cluster keeps tracks of one or more Nodes belonging to a test
+// cluster.
+type Cluster struct {
+	// Apply this timeout to any Cluster method that could
+	// possibly block indefinitely, and panic if the time expires
+	// during the method execution. It's 5 seconds by default.
+	Timeout time.Duration
+
+	// Maximum number of expect of leadership changes expected
+	// during the lifetime of the cluster. Since these changes are
+	// buffered in a channel, their upper bound must be known
+	// beforehands. The default is 1024.
+	MaxLeadershipChanges int
+
+	// Whether to automatically connect the nodes of the cluster
+	// to each other. If false, user code must manually create the
+	// connections. The default is true.
+	AutoConnectNodes bool
+
+	nodes               []*Node       // All nodes in the cluster
+	leadershipChangedCh chan *Node    // Notify leadership changes
+	output              *bytes.Buffer // Log output produced by nodes
+}
+
+// NewCluster creates a new cluster comprised of n Nodes created with
+// default raft dependencies. All LoopbackTransport instances will
+// automatically be connected to each other and the NodeStore
+// instances populated with the addresses of the other nodes.
+func NewCluster(n int) *Cluster {
+	cluster := NewUnstartedCluster(n)
+	cluster.Start()
+	return cluster
+}
+
+// NewUnstartedCluster returns a cluster whose nodes have not been
+// started yet. This allows for configuration tweaks, for instance.
+func NewUnstartedCluster(n int) *Cluster {
+	nodes := make([]*Node, n)
+	output := bytes.NewBuffer(nil)
+
+	for i := range nodes {
+		nodes[i] = NewUnstartedNode(fmt.Sprintf("%d", i), output)
+	}
+
+	return &Cluster{
+		Timeout:              5 * time.Second,
+		MaxLeadershipChanges: 1024,
+		AutoConnectNodes:     true,
+		nodes:                nodes,
+		output:               output,
+	}
+}
+
+// Start all the raft nodes in the cluster.
+func (c *Cluster) Start() {
+	if c.AutoConnectNodes {
+		connectLoobackTransports(c.nodes)
+		populateNodeStores(c.nodes)
+	}
+
+	c.leadershipChangedCh = make(chan *Node, c.MaxLeadershipChanges)
+	go c.consumeNotifyCh()
+	for _, node := range c.nodes {
+		node.Start()
+	}
+}
+
+// Shutdown all the raft nodes in the cluster.
+func (c *Cluster) Shutdown() {
+	for _, node := range c.nodes {
+		node.Shutdown()
+	}
+}
+
+// Node returns the i-th node of the cluster.
+func (c *Cluster) Node(i int) *Node {
+	return c.nodes[i]
+}
+
+// LogOutput returns the log output emitted by all nodes in the cluster.
+func (c *Cluster) LogOutput() string {
+	return c.output.String()
+}
+
+// LeadershipChanged returns the next node whose leadership state in
+// the cluster has changed, possibly blocking until it happens.
+func (c *Cluster) LeadershipChanged() *Node {
+	select {
+	case node := <-c.leadershipChangedCh:
+		return node
+	case <-time.After(c.Timeout):
+		panic("timeout waiting for leadership change")
+	}
+}
+
+// LeadershipAcquired returns the next node that has acquired
+// leadership, possibly blocking until it happens.
+func (c *Cluster) LeadershipAcquired() *Node {
+	for {
+		node := c.LeadershipChanged()
+		if node.Raft().State() == raft.Leader {
+			return node
+		}
+	}
+}
+
+// LeadershipLost returns the next node that has lost leadership,
+// possibly blocking until it happens.
+func (c *Cluster) LeadershipLost() *Node {
+	for {
+		node := c.LeadershipChanged()
+		if node.Raft().State() != raft.Leader {
+			return node
+		}
+	}
+}
+
+// Peers returns all nodes that are not the given one. This is useful
+// in combination with LeadershipAcquired to get all nodes that
+// are currently not in leader state.
+func (c *Cluster) Peers(node *Node) []*Node {
+	others := []*Node{}
+	for _, other := range c.nodes {
+		if other != node {
+			others = append(others, other)
+		}
+	}
+	return others
+}
+
+// Disconnect the network transport of the given nodes from the ther
+// nodes.
+func (c *Cluster) Disconnect(node *Node) {
+	thisTransport := loopbackTransport(node.Transport)
+	thisTransport.DisconnectAll()
+	for _, other := range c.nodes {
+		otherTransport := loopbackTransport(other.Transport)
+		otherTransport.Disconnect(node.Transport.LocalAddr())
+	}
+}
+
+// Listen for leadership change notification across all nodes, using
+// their configured NotifyCh and fill our leadershipChangedCh with the
+// node that changed its leadership state. The internal loop will run
+// forever until all NotifyCh of our peers get closed.
+func (c *Cluster) consumeNotifyCh() {
+	cases := make([]reflect.SelectCase, len(c.nodes))
+
+	for i, node := range c.nodes {
+		if node.notifyCh == nil || node.Config.NotifyCh != node.notifyCh {
+			panic(fmt.Sprintf("no valid notification channel for node %d", i))
+		}
+		cases[i] = reflect.SelectCase{
+			Dir:  reflect.SelectRecv,
+			Chan: reflect.ValueOf(node.notifyCh),
+		}
+	}
+
+	// Loop until all nodes have shutdown and closed their
+	// notifyCh.
+	for len(cases) > 0 {
+		i, _, ok := reflect.Select(cases)
+		if !ok {
+			// Remove from the select cases the notify
+			// channels that have been closed, since that
+			// means the node was shutdown.
+			cases = append(cases[:i], cases[i+1:]...)
+		}
+		c.leadershipChangedCh <- c.nodes[i]
+	}
+}
+
+// Convert a regular Transport to a LoopbackTransport, or panic if the
+// given transport does not implement LoopbackTransport.
+func loopbackTransport(transport raft.Transport) raft.LoopbackTransport {
+	loopbackTransport, ok := transport.(raft.LoopbackTransport)
+	if !ok {
+		panic("transport does not implement loopback interface")
+	}
+	return loopbackTransport
+}
+
+// Connect all the given transports to each other.
+func connectLoobackTransports(nodes []*Node) {
+	for i, node1 := range nodes {
+		for j, node2 := range nodes {
+			if i != j {
+				transport1 := loopbackTransport(node1.Transport)
+				transport2 := loopbackTransport(node2.Transport)
+				transport1.Connect(transport2.LocalAddr(), transport2)
+				transport2.Connect(transport1.LocalAddr(), transport1)
+			}
+		}
+	}
+}
+
+// Populate the node stores with the addresses of the other nodes.
+func populateNodeStores(nodes []*Node) {
+	for i := range nodes {
+		for j := range nodes {
+			if i == j {
+				continue
+			}
+			store := nodes[i].Peers
+			transport := nodes[j].Transport
+
+			addrs, err := store.Peers()
+			if err != nil {
+				panic(fmt.Sprintf(
+					"failed to get peers for node %d: %v", i, err))
+			}
+
+			store.SetPeers(append(addrs, transport.LocalAddr()))
+
+		}
+	}
+}
---
+++ cluster_test.go
@@ -0,0 +1,123 @@
+// Copyright 2017 Canonical Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rafttest_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/dqlite/raft-test"
+	"github.com/hashicorp/raft"
+)
+
+func TestCluster_LogOutput(t *testing.T) {
+	cluster := rafttest.NewCluster(3)
+	defer cluster.Shutdown()
+
+	cluster.LeadershipAcquired()
+	if output := cluster.LogOutput(); !strings.Contains(output, "[DEBUG] raft") {
+		t.Errorf("output does not seem to be a raft log:\n%s", output)
+	}
+}
+
+func TestCluster_AutoConnectWithNonLoopbackTransport(t *testing.T) {
+	cluster := rafttest.NewUnstartedCluster(2)
+	cluster.Node(0).Transport, _ = raft.NewTCPTransport("", nil, 0, time.Second, nil)
+	const want = "transport does not implement loopback interface"
+	defer func() {
+		got := recover()
+		if want != got {
+			t.Fatalf("expected panic\n%q\ngot\n%q", want, got)
+		}
+	}()
+	cluster.Start()
+}
+
+func TestCluster_PeerStoreError(t *testing.T) {
+	dir, err := ioutil.TempDir("", "go-rafttest-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	ioutil.WriteFile(filepath.Join(dir, "peers.json"), []byte("}gar![age"), 0600)
+
+	cluster := rafttest.NewUnstartedCluster(3)
+	node := cluster.Node(0)
+	node.Peers = raft.NewJSONPeers(dir, node.Transport)
+
+	const want = "failed to get peers for node 0"
+	defer func() {
+		got, ok := recover().(string)
+		if !ok {
+			t.Errorf("recover didn't return a string")
+		}
+		if !strings.Contains(got, want) {
+			t.Errorf("expected\n%q\nto contain\n%q", got, want)
+		}
+	}()
+
+	cluster.Start()
+}
+
+func TestCluster_LeadershipChanged(t *testing.T) {
+	cluster := rafttest.NewCluster(3)
+	defer cluster.Shutdown()
+
+	node := cluster.LeadershipAcquired()
+	if !node.IsLeader() {
+		t.Fatalf("node is not leader state %s", node.Raft().State())
+	}
+	cluster.Disconnect(node)
+	if cluster.LeadershipLost() != node {
+		t.Fatalf("leader node was not the one losing leadership")
+	}
+}
+
+func TestCluster_LeadershipChangedTimeout(t *testing.T) {
+	cluster := rafttest.NewCluster(3)
+	defer cluster.Shutdown()
+
+	cluster.LeadershipAcquired()
+	cluster.Timeout = time.Microsecond
+
+	const want = "timeout waiting for leadership change"
+	defer func() {
+		got := recover()
+		if want != got {
+			t.Fatalf("expected panic\n%q\ngot\n%q", want, got)
+		}
+	}()
+	cluster.LeadershipAcquired()
+}
+
+func TestCluster_LeaderKnown(t *testing.T) {
+	cluster := rafttest.NewCluster(3)
+	defer cluster.Shutdown()
+
+	leader := cluster.LeadershipAcquired()
+	leader.Raft().VerifyLeader().Error()
+	follower := cluster.Peers(leader)[0]
+	follower.LeaderKnown()
+
+	want := leader.Transport.LocalAddr()
+	got := follower.Raft().Leader()
+	if want != got {
+		t.Errorf("expected leader address\n%q\ngot\n%q", want, got)
+	}
+}
